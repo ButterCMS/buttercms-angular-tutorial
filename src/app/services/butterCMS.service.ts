0f96feb27f7d454f14fe992bf222307dbf89fa14
Vitalii <vitalij.banit@gmail.com>
2020-03-05 13:50:11 +0200

    api_key replacement

--- src/app/services/butterCMS.service.ts
+++ src/app/services/butterCMS.service.ts
@@ -1,4 +1,4 @@
 import Butter from 'buttercms';
 
-export const butterService = Butter('fef6b0175571c4faa36934f8f116dc1ce5e530d3');
+export const butterService = Butter('your_api_token');
 // replace `your_api_token` with the token provided to you by ButterCMS
