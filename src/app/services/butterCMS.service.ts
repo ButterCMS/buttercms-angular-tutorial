6b23ec3bcdf34c1175d0d7c1fb536859d061e271
Vitalii <vitalij.banit@gmail.com>
2020-03-05 13:47:14 +0200

    angular 9 upgrade

--- src/app/services/butterCMS.service.ts
+++ src/app/services/butterCMS.service.ts
@@ -1,4 +1,4 @@
-import * as Butter from 'buttercms';
+import Butter from 'buttercms';
 
-export const butterService = Butter('your_api_token');
+export const butterService = Butter('fef6b0175571c4faa36934f8f116dc1ce5e530d3');
 // replace `your_api_token` with the token provided to you by ButterCMS
